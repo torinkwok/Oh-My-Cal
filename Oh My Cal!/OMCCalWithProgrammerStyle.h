e1b652e3ffa401ca94fe0f735f9da9be86b67ce0
Tong Guo <Tong-G@outlook.com>
2014-08-28 17:11:37 +0800

    Routine backup.

--- Oh My Cal!/OMCCalWithProgrammerStyle.h
+++ Oh My Cal!/OMCCalWithProgrammerStyle.h
@@ -36,8 +36,51 @@
 // OMCCalWithProgrammerStyle class
 @interface OMCCalWithProgrammerStyle : NSView
 
-//@property ( nonatomic, assign )
+#pragma mark Buttons with bitwise operators
+@property ( nonatomic, assign ) IBOutlet NSButton* _andOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _orOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _norOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _xorOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _lshOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _rshOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _2_sOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _1_sOperator;
 
+#pragma mark Buttons with mathematical operators
+@property ( nonatomic, assign ) IBOutlet NSButton* _modOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _factorialOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _additionOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _subtractionOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _multiplicationOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _divisionOperator;
+@property ( nonatomic, assign ) IBOutlet NSButton* _lhsParenthesis;
+@property ( nonatomic, assign ) IBOutlet NSButton* _rhsParenthesis;
+@property ( nonatomic, assign ) IBOutlet NSButton* _enterOperator;
+
+#pragma mark Buttons with operands
+@property ( nonatomic, assign ) IBOutlet NSButton* _one;
+@property ( nonatomic, assign ) IBOutlet NSButton* _two;
+@property ( nonatomic, assign ) IBOutlet NSButton* _three;
+@property ( nonatomic, assign ) IBOutlet NSButton* _four;
+@property ( nonatomic, assign ) IBOutlet NSButton* _five;
+@property ( nonatomic, assign ) IBOutlet NSButton* _six;
+@property ( nonatomic, assign ) IBOutlet NSButton* _seven;
+@property ( nonatomic, assign ) IBOutlet NSButton* _eight;
+@property ( nonatomic, assign ) IBOutlet NSButton* _nine;
+@property ( nonatomic, assign ) IBOutlet NSButton* _zero;
+@property ( nonatomic, assign ) IBOutlet NSButton* _doubleZero;
+@property ( nonatomic, assign ) IBOutlet NSButton* _0xA;
+@property ( nonatomic, assign ) IBOutlet NSButton* _0xB;
+@property ( nonatomic, assign ) IBOutlet NSButton* _0xC;
+@property ( nonatomic, assign ) IBOutlet NSButton* _0xD;
+@property ( nonatomic, assign ) IBOutlet NSButton* _0xE;
+@property ( nonatomic, assign ) IBOutlet NSButton* _0xF;
+@property ( nonatomic, assign ) IBOutlet NSButton* _double0xF;
+
+#pragma mark Control Button
+@property ( nonatomic, assign ) IBOutlet NSButton* _del;
+@property ( nonatomic, assign ) IBOutlet NSButton* _clearAll;
+@property ( nonatomic, assign ) IBOutlet NSButton* _clear;
 
 @end // OMCCalWithProgrammerStyle class
 
--- Oh My Cal!/OMCCalWithProgrammerStyle.m
+++ Oh My Cal!/OMCCalWithProgrammerStyle.m
@@ -35,6 +35,53 @@
 
 // OMCCalWithProgrammerStyle class
 @implementation OMCCalWithProgrammerStyle
+
+#pragma mark Buttons with bitwise operators
+@synthesize _andOperator;
+@synthesize _orOperator;
+@synthesize _norOperator;
+@synthesize _xorOperator;
+@synthesize _lshOperator;
+@synthesize _rshOperator;
+@synthesize _2_sOperator;
+@synthesize _1_sOperator;
+
+#pragma mark Buttons with mathematical operators
+@synthesize _modOperator;
+@synthesize _factorialOperator;
+@synthesize _additionOperator;
+@synthesize _subtractionOperator;
+@synthesize _multiplicationOperator;
+@synthesize _divisionOperator;
+@synthesize _lhsParenthesis;
+@synthesize _rhsParenthesis;
+@synthesize _enterOperator;
+
+#pragma mark Buttons with operands
+@synthesize _one;
+@synthesize _two;
+@synthesize _three;
+@synthesize _four;
+@synthesize _five;
+@synthesize _six;
+@synthesize _seven;
+@synthesize _eight;
+@synthesize _nine;
+@synthesize _zero;
+@synthesize _doubleZero;
+@synthesize _0xA;
+@synthesize _0xB;
+@synthesize _0xC;
+@synthesize _0xD;
+@synthesize _0xE;
+@synthesize _0xF;
+@synthesize _double0xF;
+
+#pragma mark Control Button
+@synthesize _del;
+@synthesize _clearAll;
+@synthesize _clear;
+
 @end // OMCCalWithProgrammerStyle class
 
 //////////////////////////////////////////////////////////////////////////////
